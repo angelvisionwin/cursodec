52f629c581e5ff70de9d0d96e4bb7591f180797d
Angel Salom <52000814+angelvisionwin@users.noreply.github.com>
2021-04-25 13:38:27 +0200

    Update operadores.c

--- operadores.c
+++ operadores.c
@@ -1,16 +1,9 @@
-/**
- * @brief Multiplica 2 matrices 3x3 y devuelve el resultado
- * @param A La primera matriz a multiplicar de tamaño 3x3
- * @param B La segunda matriz a multiplicar de tamaño 3x3
- * @return Una nueva matriz C = AxB.
- */
-
 #include <stdio.h>
 int main()
 {
-    int edad;  ///&lt; Descripción resumida de a
+    int edad;  
     printf ("dime tu edad\n");
-    scanf("%d",&edad);   /*!&lt; Descripción extendida de c*/
+    scanf("%d",&edad);  
     if( edad >= 18)
     {
         printf ("eres mayor de edad\n");
